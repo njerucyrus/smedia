cb2d1ffe73ff489dafaa6d64f4c0a4b0f40696ae
njerucyrus <njerucyrus5@gmail.com>
2016-11-12 03:20:56 +0300

    added forms

---
+++ somediaapp/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class TweetForm(forms.Form):
+    product_name = forms.CharField(max_length=32, )
+    text = forms.CharField(max_length=140, widget=forms.Textarea(
+        attrs={'id': 'text_id', 'placeholder': 'Advert broadcast tweet text'}
+    ))
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", max_length=100, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', )
+
+    # remove help text in the form
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        for field_name in ['username', 'password', 'password2']:
+            self.fields[field_name].help_text = None
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=32)
+    password = forms.CharField(max_length=128, widget=forms.PasswordInput)
+
+
+class ProductForm(forms.Form):
+    product_name = forms.CharField(max_length=32)
+
+
